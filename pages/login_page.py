04ccc954ce5b6b68c03febe5a29d97cfd18592a2
Bintang <prasetyobintangg@gmail.com>
2026-07-07 18:32:34 +0700

    Implement login page object model

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.common.by import By
+
+class LoginPage:
+    
+    USERNAME_INPUT = (By.ID, "user-name")
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    
+    def __init__(self, driver):
+        self.driver = driver
+        
+    def enter_username(self, username):
+        self.driver.find_element(*self.USERNAME_INPUT).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(*self.PASSWORD_INPUT).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(*self.LOGIN_BUTTON).click()
+        
+    def login(self, username, password):
+        self.enter_username(username)
+        self.enter_username(password)
+        self.click_login()
